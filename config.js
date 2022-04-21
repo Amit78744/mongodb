8582f03cdc489c06d652962decb5ed0a9cb5a2e8
Amit78744 <amitambaliya5@gmail.com>
2022-04-21 09:54:46 +0530

    Test mongo cloud connection db

--- config.js
+++ config.js
@@ -1,8 +1,11 @@
 const mongoose = require('mongoose');
 const database = 'testmongodb';
 
-const url = "mongodb://amit:4VujvdA27MrSMkZC@cluster0-shard-00-00.rasir.mongodb.net:27017,cluster0-shard-00-01.rasir.mongodb.net:27017,cluster0-shard-00-02.rasir.mongodb.net:27017/"+database+"?ssl=true&replicaSet=atlas-flzxpn-shard-0&authSource=admin&retryWrites=true&w=majority";
+//const url = "mongodb://amit:4VujvdA27MrSMkZC@cluster0-shard-00-00.rasir.mongodb.net:27017,cluster0-shard-00-01.rasir.mongodb.net:27017,cluster0-shard-00-02.rasir.mongodb.net:27017/"+database+"?ssl=true&replicaSet=atlas-flzxpn-shard-0&authSource=admin&retryWrites=true&w=majority";
 
+//const url = "mongodb://Amit:Amit78744@13.233.30.200:27017/?authMechanism=DEFAULT"
+
+const url = 'mongodb://Amit:Amit78744@13.235.146.10:27017/?authSource=admin'
 
 async function dbConnect()
 {
